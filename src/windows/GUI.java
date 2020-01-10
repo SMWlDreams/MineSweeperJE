241e3115eca80c7200c253f7318405dfef746b5f
SMWlDreams <josephhable@hotmail.com>
2020-01-10 14:44:18 -0600

    Created initial windows

---
+++ src/windows/GUI.java
@@ -0,0 +1,15 @@
+package windows;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class GUI extends Application {
+    @Override
+    public void start(Stage stage) {
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/windows/Launcher.java
@@ -0,0 +1,7 @@
+package windows;
+
+public class Launcher {
+    public static void main(String[] args) {
+        GUI.main(args);
+    }
+}
